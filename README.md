8110c1025b0e699eca7d7d10fba7fa85bc5eb304
unknown <tomasen3@gmail.com>
2020-09-13 16:40:35 +0900

    Docker sample

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FaceCollector
+
+
+## 単体テスト
+- PowerShell(or wsl)でFaceCollectorディレクトリ配下で以下コマンドを実行
+- `docker build -t facecollector:1.0 .`
+- `docker images`
+- `docker run -p 49160:80 -d xxxx`
+- `docker exec -it xxxx /bin/bash`
+- `docker ps`
+- `curl -i localhost:49160`
---
+++ v0.1/src/server.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+
+// HTTP server init
+const express = require('express')
+const app = express()
+
+const portNo = 80
+
+app.use('/public', express.static('./public'))
+app.get('/', (req, res) => { 
+  // res.redirect(302, '/public')
+  res.send('Hello world!')
+})
+
+app.listen(portNo, () => {
+  console.log('', 'https://localhost:' + portNo)
+})
