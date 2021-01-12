82988444daa5cd65f0595fff1505f9533f6dd0db
tsutaka <tomasen3@gmail.com>
2021-01-13 00:08:21 +0900

    [add] blob container access

--- v0.1/src/server.js
+++ v0.1/src/server.js
@@ -11,6 +11,9 @@ const newItem = {
   "status": "new"
 }
 
+// Storage
+const { BlobServiceClient } = require('@azure/storage-blob')
+
 // HTTP server init
 const express = require('express')
 const app = express()
@@ -22,11 +25,42 @@ app.get('/', (req, res) => {
   res.redirect(302, '/public')
 })
 
-app.get('/video-list', (req, res) => { 
-
+app.get('/video', (req, res) => { 
+  console.log('video!')
+  getContainer().catch((err) => {
+    console.error("Error:", err.message)
+  })
   res.send('video check!')
 })
 
 app.listen(portNo, () => {
-  console.log('', 'https://localhost:' + portNo)
+  console.log('', 'http://localhost:' + portNo)
 })
+
+async function getContainer(){
+  // set connectionstring
+  // case windows
+  // setx AZURE_STORAGE_CONNECTION_STRING "<yourconnectionstring>"
+  // const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING
+  // case direct
+  const AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=fcvideostorage;AccountKey=6HhPHpXvepXYa153+s5rvSakaS/6q0FFtqr741vXGxZVf8pbGD+gxgfELR0jGKo2V3BIMVZUUl+prClkULwyqw==;EndpointSuffix=core.windows.net"
+  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING)
+
+  const containerName = 'fcinputvideo'
+  const containerClient = blobServiceClient.getContainerClient(containerName)
+
+  let i = 1
+  let blobs = containerClient.listBlobsFlat()
+  for await (const blob of blobs) {
+    console.log(`Blob ${i++}: ${blob.name}`)
+    if(blob.name.match(/.mp4/)){
+      console.log(`mp4! Downloading...`)
+      const blockBlobClient = containerClient.getBlockBlobClient(blob.name)
+      // const downloadBlockBlobResponse = await blockBlobClient.download(0)
+      // const str = await streamToString(downloadBlockBlobResponse.readableStreamBody)
+      const response = await blockBlobClient.downloadToFile("./" + blob.name)
+      console.log(`Download done!`)
+    }
+  }
+  return 0
+}
